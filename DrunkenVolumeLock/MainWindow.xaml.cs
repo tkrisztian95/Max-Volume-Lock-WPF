2cab0fa93e54520982e88ef726f0d6bb66769f53
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2018-09-23 22:05:44 +0200

    Use NAudio +Detect volume change event

--- DrunkenVolumeLock/MainWindow.xaml.cs
+++ DrunkenVolumeLock/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,9 @@ namespace DrunkenVolumeLock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string TEXT_BTN_LOCK = "Lock it";
+        private const string TEXT_BTN_UNLOCK = "Unlock it";
+
         private const string ICO_LOCK_PATH = "Assets/lock.ico";
         private const string ICO_UNLOCK_PATH = "Assets/unlock.ico";
         private const int DEFAULT_MAX_VOL = 90;
@@ -48,6 +52,8 @@ namespace DrunkenVolumeLock
             }
         }
 
+        private MMDevice dev;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -56,8 +62,14 @@ namespace DrunkenVolumeLock
         protected override void OnInitialized(EventArgs e)
         {
             base.OnInitialized(e);
-            VolumeActual = (int)Math.Truncate(AudioManager.GetMasterVolume());
+
+            NAudio.CoreAudioApi.MMDeviceEnumerator enumer = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+            dev = enumer.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            dev.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification;
+
+            VolumeActual = (int)(dev.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
             VolumeMax = DEFAULT_MAX_VOL;
+
             slider_maxVol.Value = VolumeMax;
             picker_endTime.Value = DateTime.Now.AddHours(2);
         }
@@ -67,15 +79,19 @@ namespace DrunkenVolumeLock
             VolumeMax = (int)e.NewValue;
         }
 
-        private void lockIt()
+        private void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
         {
-            img_lockUnlock.Source = new BitmapImage(new Uri(ICO_LOCK_PATH, UriKind.Relative));
-            IsLocked = true;
-        }
-        private void unlockIt()
-        {
-            img_lockUnlock.Source = new BitmapImage(new Uri(ICO_UNLOCK_PATH, UriKind.Relative));
-            IsLocked = false;
+            Dispatcher.Invoke(() =>
+            {
+                float volume = (data.MasterVolume * 100);
+                VolumeActual = (int)volume;
+            });
+
+            if (IsLocked && VolumeActual > VolumeMax)
+            {
+                float volume = VolumeMax / 100f;
+                dev.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+            }
         }
 
         private void btn_lockMaxVol_Click_1(object sender, RoutedEventArgs e)
@@ -83,5 +99,24 @@ namespace DrunkenVolumeLock
             if (IsLocked) { unlockIt(); }
             else { lockIt(); }
         }
+
+
+        private void lockIt()
+        {
+            img_lockUnlock.Source = new BitmapImage(new Uri(ICO_LOCK_PATH, UriKind.Relative));
+            IsLocked = true;
+            slider_maxVol.IsEnabled = false;
+            chb_pinRequired.IsEnabled = false;
+            btn_lockMaxVol.Content = TEXT_BTN_UNLOCK;
+        }
+        private void unlockIt()
+        {
+            img_lockUnlock.Source = new BitmapImage(new Uri(ICO_UNLOCK_PATH, UriKind.Relative));
+            IsLocked = false;
+            slider_maxVol.IsEnabled = true;
+            chb_pinRequired.IsEnabled = true;
+            btn_lockMaxVol.Content = TEXT_BTN_LOCK;
+        }
+
     }
 }
