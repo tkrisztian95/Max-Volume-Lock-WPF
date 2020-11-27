91d6ca3d824c05cb9c2c45aef91f69ec83d293c8
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2020-11-27 15:41:11 +0100

    Pin input should anly accept numeric input

--- MaxVolumeLock/PinWindow.xaml.cs
+++ MaxVolumeLock/PinWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -75,6 +76,15 @@ namespace MaxVolumeLock
             }
             mainWindow.toggleVolumeLock();
             this.Close();
+        }
+
+        private void pinInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[0-9]+");
+            if (!regex.IsMatch(e.Text))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
